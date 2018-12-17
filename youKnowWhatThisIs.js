4840e0ef95a3ddd48b0db0da9a30f756c8fd340c
willPower777 <rickbosco@gmail.com>
2018-12-16 23:08:38 -0700

    Made updated changes locally and didn't have setup in past.

--- youKnowWhatThisIs.js
+++ youKnowWhatThisIs.js
@@ -6,14 +6,14 @@ var contestantName = function(){return window.prompt("What is your name valiant 
 var readyFriend = function(){return window.confirm("Have you asked someone nicely?");};
 var friendName = function(){return window.prompt("What is your friend's name? Please enter now.\n"+
 	"(And PLEASE put in a regular name.)");};
-var gameplayRules1 = function(){return window.confirm("Alright, your friend "+ nemesis +" will need to"
+var gameplayRules1 = function(){return window.confirm("Alright, your friend "+ nemesis +" will need to "
 	+"pick 4 letters from A-F.\n They could be all the same (DDDD) or any mix in any order. For example:\n"+
 	"(AAFF) (ABCD) (EAAE) (DCBA)\n Once they enter the code, you will have 12 attempts to\n"+
 	"figure it out! Do you get it so far?");};
 var gameplayRules2 = function(){return window.confirm("Okay so once the code is entered I will give you feedback on your attempts!\n"+
-	"For instance if the winning code is (AADD) and you entered (DACC)\n I will say 'you have 1 correct placement' and"+
-	"'you have 1 correct letter'\n Keep in mind placement will imply correct letter. Just 'letter' means not\n"+
-	"the right placement but a correct letter in the code. But you are on the right track.\n Do you understand?");};
+	"   For instance if the winning code is (AADD) and you entered (DACC)\n I will say 'you have 1 correct placement' and"+
+	"'separately you have 1 correct letter.'\n   Keep in mind placement will imply correct letter. Just 'letter' means not\n"+
+	"the right placement but a correct letter in the code. But you are on the right track.\n\n Do you understand?");};
 var winCondition = function(){return window.prompt("Alright friend what is your code? Please keep it to 4 letters.\n"+
 	"Between A-F. -Or you'll be hearing from me again.");};
 var playersTurn = function(){return window.prompt("So, "+playerName+" what is your first guess? Remember:\n"+
@@ -24,6 +24,7 @@ var enteredCode;
 var codeArray=[];
 var playerName;
 var previousGuesses = [];
+var newPreviousGuesses = [];
 
 var checkCodeLength = function(enteredCode){
 			while (enteredCode.length!=4){
@@ -57,7 +58,7 @@ var checkCodeCondition = function(enteredCode){
 	//Following is to check if entry is between A-F used to try enteredCode.charAt(i) but strings are inmutable
 	//Also below I originally used || to check but if it was an A than the following were true and causing probs
 	//TO DO - Look into making array for below set to shorten this up perhaps?
-	for (var i=0; i< codeArray.length; i++){
+	for (var i=0; i < codeArray.length; i++){
 		if (codeArray[i]!='A' &&
 			codeArray[i]!='B' &&
 			codeArray[i]!='C' &&
@@ -70,8 +71,11 @@ var checkCodeCondition = function(enteredCode){
 			codeArray[i]!='D' &&
 			codeArray[i]!='E' &&
 			codeArray[i]!='F'){
+				console.log(codeArray[i]+"<--that was current i for codeArray");
 			codeArray[i] = window.prompt("It appears your "+(i+1)+" entry is not between A-F\n"+
 				"Or maybe you put in a symbol... tsk tsk...\nPlease enter that letter now.");
+			codeArray[i]= codeArray[i].toUpperCase();
+			alert("You know have entered this: "+codeArray);
 			};
 		}
 	}
@@ -103,7 +107,7 @@ function mainMind(){
 		while (prepFriend != true){
 			alert("Try to get them this time!");
 			prepFriend = readyFriend();
-		} 
+		}
 		nemesis = friendName();
 		while (nemesis.match(/\d+/g) != null){
 			nemesis = window.prompt("Alright try that name of your friend again without the numbers!");
@@ -122,76 +126,81 @@ function mainMind(){
 			alert("Alrighty then; on with the game!");
 		};
 		var winningCode = winCondition();
-		console.log(winningCode+" preClean");
+		// console.log(winningCode+" preClean");
 		//above call may be redundant. May just need below line. And or it needs to be assigned a variable?
 		winConditionClean = checkCodeCondition(winningCode);
-		console.log(winConditionClean+" postClean");
+		// console.log(winConditionClean+" postClean");
 		alert("Now it's time for the player to guess. You have at most 12 chances.");
 		var playersGuess = playersTurn();
-		console.log(winConditionClean+" right after playersGuess happens")
+		// console.log(winConditionClean+" right after playersGuess happens")
 		//previous calls the variable out and player enters. Line below should clean it up like the other.
 		var timeToPlay = function(){
 			for (t=1; t<=12; t++){
-				console.log(winConditionClean+" just before reenter of new code")
+				// console.log(winConditionClean+" just before reenter of new code")
 				var playersTurnClean = checkCodeCondition(playersGuess);
-				console.log(playersTurnClean+" now this is the players entryCleaned");
-				console.log(t +"this is t turn");
-				previousGuesses.push(playersTurnClean+"\n");
-				console.log(winConditionClean+" winning statementClean");
+				// console.log(playersTurnClean+" now this is the players entryCleaned");
+				// console.log(t +"this is t turn");
+				// previousGuesses.push(playersTurnClean+"\n");
+				// console.log(winConditionClean+" winning statementClean");
 				// console.log(playersTurnClean+" now this is the players entry after the push");
 				// alert("this the win condition "+winConditionClean+"\nthis the play turn "+playersTurnClean);
 				if ((winConditionClean[0] == playersTurnClean[0])&&(winConditionClean[1] == playersTurnClean[1])&&
-					(winConditionClean[2] == playersTurnClean[2])&&(winConditionClean[3] == playersTurnClean[3])&& (t=1)){
+					(winConditionClean[2] == playersTurnClean[2])&&(winConditionClean[3] == playersTurnClean[3])&& (t==1)){
 					return alert("You WON! You made it even on your first try! WOOHOO!\n"+
 						"The code was: "+winConditionClean+"\n Your winning guess was: "+playersTurnClean);
 				} else if ((winConditionClean[0] == playersTurnClean[0])&&(winConditionClean[1] == playersTurnClean[1])&&
 					(winConditionClean[2] == playersTurnClean[2])&&(winConditionClean[3] == playersTurnClean[3]) && (t==12)) {
-					return alert("WHEW! ...You finally won! You got it right after only "+t+" times! Yay!!\n"+
+					return alert("WHEW! ..."+playerName+" You finally won! You got it right after only "+t+" times! Yay!!\n"+
 						"The code was: "+winConditionClean+"\n Your winning guess was: "+playersTurnClean+ "\n"+
-						"Your previous guesses were: " + previousGuesses);
+						"Your previous guesses were:\n" + newPreviousGuesses);
 				} else if ((winConditionClean[0] == playersTurnClean[0])&&(winConditionClean[1] == playersTurnClean[1])&&
 					(winConditionClean[2] == playersTurnClean[2])&&(winConditionClean[3] == playersTurnClean[3])) {
-					return alert("You got it! After only "+t+" tries!\n"+
+					return alert(playerName+" YOU GOT IT!! You beat "+nemesis+" After only "+t+" tries!\n"+
 						"The code was: "+winConditionClean+"\n Your winning guess was: "+playersTurnClean+ "\n"+
-						"Your previous guesses were: " + previousGuesses);
+						"Your previous guesses were:\n" + newPreviousGuesses);
 				} else {
 					var inPlaceLetter=0;
 					var rightLetter=0;
-					var winConditionPluck=winConditionClean;
-					console.log(winConditionClean+" Clean right after dec of winConditionPluck");
-					var playersTurnPluck=playersTurnClean;
+					var winConditionPluck=winConditionClean.slice(0);
+					// console.log(winConditionClean+" Clean right after dec of winConditionPluck");
+					// console.log(winConditionPluck+ " winPluck right after the dec of pluc");
+					var playersTurnPluck=playersTurnClean.slice(0);
 					for(i=0; i<playersTurnClean.length; i++){
 						if (playersTurnClean[i]==winConditionClean[i]){
 							inPlaceLetter++;
 							var winConditionPulled=winConditionPluck.splice(winConditionPluck.indexOf(playersTurnClean[i]),1);
 							var playersTurnPulled=playersTurnPluck.splice(playersTurnPluck.indexOf(winConditionClean[i]),1);
-							console.log(winConditionClean +" Clean right after the splice");
-						} 
+							// console.log(winConditionClean +" Clean right after the splice");
+							// console.log(winConditionPluck+ " winPluck right after the splice");
+						}
 					}
-					//---NEED To figure out below how to get to check against all in an array. line 149
 					//--added winConditionPluck to be able to pull a checked value out of the array to not doublecheck
 					for(j=0; j<playersTurnPluck.length; j++){
 						// console.log(playersTurnClean[i]);
-						// console.log(winConditionPluck+" this is pluck total");
+						// console.log(winConditionPluck+" this is win pluck total");
+						// console.log(playersTurnPluck+" this is players pluck total");
 						// console.log(winConditionPluck[j]+" this is pluck single "+j+" time through this player guess: " +playersTurnClean[j]);
 						// console.log(winConditionPluck.indexOf(playersTurnClean[j])+" That is indexOf pluck array based on players guess letter");
-						if ((playersTurnPluck[j]!=winConditionPluck[j]) && 
-							(winConditionPluck.indexOf(playersTurnPluck[j])!=-1)){
+						if ((winConditionPluck.indexOf(playersTurnPluck[j])!=-1)){
 							rightLetter++;
-							// console.log(rightLetter +" this time"+j)
+							// console.log(rightLetter +" this time of players Pluck"+j)
 							var winConditionPulled2=winConditionPluck.splice(winConditionPluck.indexOf(playersTurnPluck[j]),1);
+							// console.log(winConditionPluck+ " winPluck right after the second splice");
 						}
 					}
+					previousGuesses.push(t+". "+playersTurnClean+" with "+inPlaceLetter+" correct placement and separately "+
+						rightLetter + " right letter(s) not spot.\n");
+					newPreviousGuesses=previousGuesses.join("");
 					playersGuess=window.prompt("Okay you didn't get it with that last guess.\n"+
 						"You have "+inPlaceLetter+" correct letter(s) in the right placement!!\n"+
-						"You got "+rightLetter+" letter(s) in the code but not in the right spot.\n"+
-						"Your previous guesses were: \n"+ previousGuesses+ "  \nNow you should try again!"+
+						"You got separately "+rightLetter+" letter(s) in the code but not in the right spot.\n"+
+						"Your previous guesses were: \n"+ newPreviousGuesses+ "  \nNow you should try again!"+
 						" You have " +(12-t)+ " tries left!");
 				}
 			};
-			return alert("Sadly, it would appear that "+contestantName+"\nhas been bested by " +friendName+"!\n"+
-				"The code from "+friendName+" was: "+winConditionClean+"\n"+ 
-				"Your previous guesses were: " + previousGuesses);
+			return alert("Sadly, it would appear that "+playerName+"\nhas been bested by " +nemesis+"!\n"+
+				"The code from "+nemesis+" was: "+winConditionClean+"\n"+
+				"Your previous guesses were:\n" + newPreviousGuesses);
 
 		};
 		timeToPlay();
@@ -199,3 +208,6 @@ function mainMind(){
 	}
 }
 mainMind();
+
+// (playersTurnPluck[j]!=winConditionPluck[j]) &&
+// above was in the loop for deciding the right letter right after the if
